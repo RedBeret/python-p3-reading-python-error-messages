2c87af000b31affa2dac98ae73ca6aa03583ff09
Steven Espinoza <espisteven5@gmail.com>
2023-12-11 10:48:00 -0800

    done

--- lib/a_syntax_error.py
+++ lib/a_syntax_error.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
-poor_syntax = 2 * #
+
+count = 1
+while count < 100:
+    count += 1
+    print("%i" % count)
--- lib/an_assertion_error.py
+++ lib/an_assertion_error.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-assert(1 == 2)
+
+assert(1 == 1)
+
+
+my_list = [0, 1, 2, 3, 4]
+# my_list[10]
+
+
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+# my_dict['d']
+flatiron_school = "Flatiron School"
+
+print(flatiron_school)
+
+wrong_type = 'abc' + str(123)
